f61ad64418f6754fc12028d49516b0c6a0679239
divy9t12@icloud.com <divy9t12@icloud.com>
2021-04-27 16:19:32 +0530

    updating manifest file

--- partner_product/__manifest__.py
+++ partner_product/__manifest__.py
@@ -3,15 +3,14 @@
     'name': "partner_product",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Partner product relation""",
 
     'description': """
-        Long description of module's purpose
+        The module to check how many products are sold to which customer
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Trimis Tech",
+    'website': "",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
@@ -20,7 +19,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base','stock','sale_management'],
 
     # always loaded
     'data': [
